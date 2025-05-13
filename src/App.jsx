913ad520c4d586b7ccd42b85483f1f82defabc15
muktijindal <muktijindal@devnexus.in>
2025-05-13 14:29:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  return (
+ <div className='bg-amber-200'>
+  <section>
+    scbsjd
+    kbshef
+  </section>
+  
+  Hello
+ </div>
+  )
+}
+
+export default App;

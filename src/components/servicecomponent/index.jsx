986b1bf2dfedf077b360ea9f18b7439289ecb98a
Mukti jindal <muktijindal@devnexus.in>
2025-06-11 14:19:20 +0530

    Update index.jsx servicesection

--- src/components/servicecomponent/index.jsx
+++ src/components/servicecomponent/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Banners } from "../global/NavBanners";
-import BannerImage1 from "../../../public/images/services/servicesHeroSection.jpg";
+// import { Banners } from "../global/NavBanners";
+// import BannerImage1 from "../../../public/images/services/servicesHeroSection.jpg"; 
 import OurProcess from "./Process";
 import ServicesSection from "./ServicesSection";
 
@@ -9,13 +9,13 @@ import ServicesSection from "./ServicesSection";
 export const ServiceComponent = () => {
   return (
     <div>
-      <Banners
+{/*       <Banners
         image={BannerImage1}
         overlayImage
         heading="Our Services"
         description="At DevNexus Solutions, we are offering a full
                                        range of IT services that are designed to upgrade your brand and drive business growth."
-      />
+      /> */}
       <ServicesSection />
       <OurProcess />
     </div>
